ac5e47e56ec2d68085402a97f94f912ac6c847d1
vivektarun <vivektarun1234@gmail.com>
2025-08-04 16:58:40 +0530

    feat(idempotentApi): create inmemory DB & make payment idempotent

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -3,6 +3,8 @@ const { SuccessResponse, ErrorResponse } = require('../utils/common');
 const { BookingService } = require('../services');
 const { error } = require('../utils/common/error-response');
 
+const inMemDb = {};
+
 async function createBooking(req, res) {
     try {
         // Validate the incoming payload
@@ -33,11 +35,23 @@ async function createBooking(req, res) {
 
 async function makePayment(req, res) {
     try {
+        const idempotencyKey = req.headers['x-idempotency-key'];
+
+        if(!idempotencyKey) {
+            return res.status(StatusCodes.BAD_REQUEST).json({message: 'Idempotency key is missing'});
+        }
+
+        if(inMemDb[idempotencyKey]) {
+            return res.status(StatusCodes.BAD_REQUEST).json({message: 'Cannot retry on a successful payment'});
+        }
+
         const response = await BookingService.makePayment({
             totalCost : req.body.totalCost,
             userId: req.body.userId,
             bookingId: req.body.bookingId
         });
+
+        inMemDb[idempotencyKey] = idempotencyKey;
 
         SuccessResponse.data = response;
         return res.status(StatusCodes.OK).json(SuccessResponse);
